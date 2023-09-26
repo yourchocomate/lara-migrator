156b9760d67fb97ffc220bc84fb5881612661ded
Habibur Rahman <yourchocomate@gmail.com>
2023-09-26 23:50:52 +0600

    ✨ added primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4ecca3"
+      },
+    },
   },
   plugins: [],
 }
