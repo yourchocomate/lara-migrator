453f925256fb1b52de959c4674eac5bda5ed1f09
Md Habibur Rahman <yourchocomate@gmail.com>
2024-02-21 13:39:49 +0600

    changed base to (./)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: './',
     resolve: {
         alias: {
             "@lib": resolve(__dirname, "lib"),
